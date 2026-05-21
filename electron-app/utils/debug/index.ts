f5769494bb13df1571085817133e058322976665
Nick2bad4u <20943337+Nick2bad4u@users.noreply.github.com>
2026-05-21 08:19:14 -0400

    sparkles [feat] Migrate debug barrel exports to TypeScript

--- electron-app/utils/debug/index.ts
+++ electron-app/utils/debug/index.ts
@@ -6,10 +6,12 @@ import * as debugChartFormatting from "./debugChartFormatting.js";
 import * as debugSensorInfo from "./debugSensorInfo.js";
 import * as lastAnimationLog from "./lastAnimLog.js";
 import * as stateDevTools from "./stateDevTools.js";
+
 export * from "./debugChartFormatting.js";
 export * from "./debugSensorInfo.js";
 export * from "./lastAnimLog.js";
 export * from "./stateDevTools.js";
+
 export default {
     ...debugChartFormatting,
     ...debugSensorInfo,
